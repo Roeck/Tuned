2a3e5f5b799ef0945eb19a20f53b95276be04810
Roeck <rons9n@gmail.com>
2020-11-29 17:49:09 -0500

    LibrarySong component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React,{useState} from 'react';
 import "./styles/app.scss";
 import Player from './components/Player'
 import Song from './components/Song'
+import Library from './components/Library'
+import LibrarySong from './components/LibrarySong'
 import data from './data';
 
 function App() {
@@ -17,6 +19,7 @@ function App() {
         isPlaying={isPlaying}
         currentSong={currentSong}
       />
+      <Library songs={songs} />
     </div>
   );
 }
